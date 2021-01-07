0c0d1e0993322cbb8a81484ea5ba36004bc1be34
Dave Turner <davidt0x@gmail.com>
2021-01-07 02:22:18 -0500

    Converted hidapi port to use experimental hidapi cmake build

--- ports/hidapi/portfile.cmake
+++ ports/hidapi/portfile.cmake
@@ -1,32 +1,27 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libusb/hidapi
-    REF fc677b81951d8facc99bab6b4f0060b5d89e2e15 # use commit from master for windows/hidapi.vcxproj
-    SHA512 8a779c1d4fe83e264046f3193a5cefe2d9765dcde30628767838180b3dec2bdd25c9c1ec9a96b3a7edaf00df7662b4d658f2b57bda67cebc7d7cb4e737cb1f88
-    HEAD_REF master
+    REF ad7cc64ee1fce66806dd471216dbdc8555557e5b # use commit from cmake-support
+    SHA512 5fd1e49538308c3d9146be1ca32bcf5f2c88a19f3240ee80ad7f6aceace4bc185c558bbce0b71ddf7029b95fc3b37cf805e8a3af23c4f69126666d1a1a10af06
+    HEAD_REF cmake-support
+    PATCHES 
+    	"move-include-gnu-install-dirs.patch"
 )
 
-if(VCPKG_TARGET_IS_WINDOWS)
-    if(TRIPLET_SYSTEM_ARCH MATCHES "arm")
-        message(FATAL_ERROR "ARM builds are currently not supported!")
-    elseif(VCPKG_CMAKE_SYSTEM_NAME STREQUAL WindowsStore)
-        message(FATAL_ERROR "UWP builds are currently not supported!")
-    endif()
+vcpkg_configure_cmake(
+	SOURCE_PATH ${SOURCE_PATH}
+	PREFER_NINJA
+)
+	
+vcpkg_install_cmake()
 
-    vcpkg_install_msbuild(
-        SOURCE_PATH ${SOURCE_PATH}
-        PROJECT_SUBPATH windows/hidapi.sln
-        INCLUDES_SUBPATH hidapi ALLOW_ROOT_INCLUDES
-        LICENSE_SUBPATH LICENSE-bsd.txt # use BSD license
-    )
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 
-    file(COPY
-        ${CMAKE_CURRENT_LIST_DIR}/hidapi-config.cmake
-        DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
-    if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-        file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-    endif()
-else()
-    message(FATAL_ERROR "Non-Windows builds are currently not supported!")
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+	file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
